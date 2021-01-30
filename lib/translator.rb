d671b0b1742511d86419216e202ef08851041922
Shane R <imspammails@gmail.com>
2021-01-30 00:15:15 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -22,7 +22,7 @@ def get_japanese_emoticon(ymlpage, emoticon)
   # code goes here
   emoticons = load_library('./lib/emoticons.yml')
   emoticons.each_with_object({}) do |(key, value)|
-    emoticons.dig(key, :english)
+    return emoticons.dig(key, :english)
     #binding.pry
   end
 end
