57b4c94932fd6a494e42a5e7e7f46106fe17753a
Shane R <imspammails@gmail.com>
2021-01-30 00:31:02 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -27,7 +27,7 @@ def get_japanese_emoticon(ymlpage, emoticon)
     if emoticons.dig(key, :english) == emoticon
       j_emoji = emoticons.dig(key, :japanese)
     end
-    
+    binding.pry
   end
   return j_emoji
 end
